f130150a09f14088a717c076c3bf10cf0977092a
Erika <erikacardon@gmail.com>
2024-03-09 11:18:15 -0600

    Updates

---
+++ static/NwOsR-XnaZNavHsqm5Uow/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/bee240a3-bf5787d4a1a9c90a.js","static/chunks/46-bd6ac5b1bf2ad8d8.js","static/chunks/pages/index-9cbccad16b485355.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
